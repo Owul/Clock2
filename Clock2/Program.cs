d5d8607a769619153ddf52232ce1136a0b236ea4
scorp <scorp@DESKTOP-72D584C>
2024-03-10 11:18:07 +0200

    Add project files.

---
+++ Clock2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Clock2
+
+{
+    public class Clock
+    {
+        private int hour;//0-23 hours
+        private int minute;//0-59 min
+        private int minutesInDay = 24 * 60;
+
+        public Clock(int hour, int minute)
+        {
+            this.hour = hour;
+            this.minute = minute;
+        }
+
+        public int getInterval() // תשלים את המתודה
+        {
+            int minutesInDay = 60 * 24;
+            int currTimeToMinutes = 60 * hour + minute;
+            return minutesInDay - currTimeToMinutes;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Clock seif1Example = new Clock(20, 45);// דוגמה לסעיף הראשון לראות שיצא התוצאה שנתנו בדוגמה
+            Console.WriteLine(seif1Example.getInterval());
+        }
+    }
+}
